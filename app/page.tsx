a04162e9063acbd2ad2d02f25bdba6c9540814a9
Hasan Cemil Acar <70777645+rentonhead@users.noreply.github.com>
2023-09-04 19:33:12 +0300

    some changes -rentboy

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,12 @@ export default function Home() {
       <div className="items-center space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
         <div className="flex flex-col items-center pt-8">
           <Image
-            alt="Picture of jan marshal"
+            alt="Picture of hasan cemil acar"
             src={Me}
+            width={500}
+            height={500}
+            priority
+            placeholder="blur"
             className="h-48 w-48 rounded-full object-cover object-top"
           />
           <h1 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
@@ -43,7 +47,7 @@ export default function Home() {
                 <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1168.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z" />
               </svg>
             </a>
-            <a href="mailto:hasancemilacar@gmail.com?subject=Next.js Hakkında">
+            <a href="mailto:hasancemilacar@gmail.com?subject=Hey,rentonhead!">
               <svg
                 viewBox="0 0 1024 1024"
                 fill="currentColor"
